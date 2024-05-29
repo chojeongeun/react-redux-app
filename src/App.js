9880e81b398cbb93049911d0a71decf29f3ad1b4
chojeongeun <juy0816@naver.com>
2024-05-29 16:32:03 +0900

    리액트 컴포넌트에서 리덕스 스토어에 있는 상태 사용할 수 있게 해주기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+
+function App({value ,onIncrement, onDecrement}) {
+  console.log(value)
+  const [todoValue, setTodoValue] = useState('')
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      <ul>
+
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <input type='text' 
+          value={todoValue} 
+          onChange={(e)=>setTodoValue(e.target.value)}
+          />
+        <input type='submit' />
+      </form>
+    </div>
+
+     Clicked:{value.counter} times
+     {" "}
+     <button onClick={onIncrement}>
+      +
+     </button>
+     {" "}
+     <button onClick={onDecrement}>
+      -
+     </button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,13 +3,36 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import counter from './reducers';
+import rootReducer from './reducers';
+import { Provider } from 'react-redux';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const store = createStore(rootReducer);
+
+console.log(store.getState())
+
+store.dispatch({
+  type: 'ADD_TODO',
+  text: '밥먹기'
+})
+
 root.render(
   <React.StrictMode>
-    <App />
+   <Provider store={store}>
+     <App 
+     value={store.getState()} 
+     onIncrement={()=>store.dispatch({type:'INCREMENT'})} 
+     onDecrement={()=>store.dispatch({type:'DECREMENT'})}
+     />
+   </Provider>
   </React.StrictMode>
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
